77dd2ae028324bae1883332d3da0fd49b66a4a21
frontendprof <https://github.com/frontendprof>
2021-06-29 21:01:29 +0500

    styling the layout with bootstrap

--- src/App.js
+++ src/App.js
@@ -7,9 +7,17 @@ import "bootstrap/dist/js/bootstrap.js"
 
 function App() {
   const {t}=useTranslation();
+  const release_day=new Date("2021-03-07");
+  const time_diff=new Date() - release_day;
+  const number_of_days=Math.floor(time_diff/(1000*3600*24))
+
   return (
-    <div className="App">
-      <h2>{t("trans")}</h2>
+    <div className="container">
+      <div className="d-flex flex-column align-items-start">
+        <h2 className="mt-3">{t("welcome_message")}</h2>
+        <p>{t("days_since_release", {number_of_days})}</p>
+
+      </div>
     </div>
   );
 }
